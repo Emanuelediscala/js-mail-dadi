e0cb41e483663557d401a627ae7df6c936d71d66
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-18 17:39:26 +0200

    indentazione

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 let richiestamail = prompt("inserisci la tua mail")
-let listamailvalide = ["nicola.mutti@libero.it","saverio.sasso@tiscali.it","luca.rossi@gmail.com","manueldiscala@gmail.com"]
+let listamailvalide = ["nicola.mutti@libero.it", "saverio.sasso@tiscali.it", "luca.rossi@gmail.com", "manueldiscala@gmail.com"]
 
 let mailvalida = false
 
 for (let c = 0; c < listamailvalide.length; c++) {
 
     if (richiestamail == listamailvalide[c]) {
-    mailvalida = true 
-} 
-}
-    if (mailvalida == true) {
-        alert("la tua mail è stata riconosciuta")
+        mailvalida = true
     }
-    else 
-        alert("la tua mail non è stata riconosciuta")
+}
+if (mailvalida == true) {
+    alert("la tua mail è stata riconosciuta")
+}
+else
+    alert("la tua mail non è stata riconosciuta")
 
 
