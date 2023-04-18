a2d8f35d0e126d61ebff55b9188f9afd840b717c
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-18 17:33:20 +0200

    feat( logic,responsive) must be definied in html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 let richiestamail = prompt("inserisci la tua mail")
 let listamailvalide = ["nicola.mutti@libero.it","saverio.sasso@tiscali.it","luca.rossi@gmail.com","manueldiscala@gmail.com"]
 
+let mailvalida = false
+
 for (let c = 0; c < listamailvalide.length; c++) {
-    
+
     if (richiestamail == listamailvalide[c]) {
-        alert("la tua mail è stata riconosciuta")
-    } 
-        alert("Mail  non riconosciuta")
+    mailvalida = true 
+} 
 }
+    if (mailvalida == true) {
+        alert("la tua mail è stata riconosciuta")
+    }
+    else 
+        alert("la tua mail non è stata riconosciuta")
+
+
