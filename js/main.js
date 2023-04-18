3fc7ba7be4f9db7be30e2d6738d1c01619345535
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-18 15:49:04 +0200

    modifica Array

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 let richiestamail = prompt("inserisci la tua mail")
+let listamailvalide = ["nicola.mutti@libero.it","saverio.sasso@tiscali.it","luca.rossi@gmail.com","manueldiscala@gmail.com"]
 
-let listamailvalide = ["nicola.mutti@libero.it,saverio.sasso@tiscali.it,luca.rossi@gmail.com,manueldiscala@gmail.com"]
-
-for (let c = 0; c > listamailvalide.length; c++) {
+for (let c = 0; c < listamailvalide.length; c++) {
     
-    
-
-    if (richiestamail == listamailvalide [c]) {
+    if (richiestamail == listamailvalide[c]) {
         alert("la tua mail è stata riconosciuta")
     } 
         alert("Mail  non riconosciuta")
