0b355d940bbc8d1eb8d325a02af9dd7d53c11c6f
Emanuele Di Scala <manueldiscala@gmail.com>
2023-04-18 15:20:08 +0200

    feat:(prompt, and for)

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let richiestamail = prompt("inserisci la tua mail")
+
+let listamailvalide = ["nicola.mutti@libero.it,saverio.sasso@tiscali.it,luca.rossi@gmail.com,manueldiscala@gmail.com"]
+
+for (let c = 0; c > listamailvalide.length; c++) {
+    
+    
+
+    if (richiestamail == listamailvalide [c]) {
+        alert("la tua mail è stata riconosciuta")
+    } 
+        alert("Mail  non riconosciuta")
+}
